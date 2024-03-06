6f162ad94700e857d97ef687f8abfa19c5a9da4e
dg1223 <alavi1223@hotmail.com>
2024-03-06 04:29:20 -0500

    feature: added failsafe to prevent reuse of stale word count

--- GSoC/initial_task.py
+++ GSoC/initial_task.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import os
+import shutil
 import sys
+import time
 from PyPDF2 import PdfReader
 
 if len(sys.argv) != 2:
@@ -31,12 +33,41 @@ j = Job(name=job_name)
 j.application = Executable()
 j.application.exe = File('run_initial_task.sh')
 
-# Create splitter and merger for the word count job
+# Create splitter for the word count job
 if job_name == 'count_it':
     reader = PdfReader('LHC.pdf')
     number_of_pages = len(reader.pages)
     word = 'it'
+    # split
     splitter_args = [ [current_dir, page_num, word] for page_num in range(number_of_pages)]
     j.splitter = ArgSplitter(args=splitter_args)
 
 j.submit()
+
+
+'''
+Failsafe:
+Store the final count to a different file and 
+remove the earlier file so that re-running the job
+does not use the count from a previously run job
+'''
+if job_name == 'count_it':
+    start_time = time.time()
+    timeout = 60 # 1 minute
+
+    while j.status != 'completed':
+        # timeout if job is still running, probably an issue
+        if time.time() - start_time > timeout:
+            print("Timeout reached. Not waiting anymore for job completion")
+            break
+
+        continue
+
+    output_file = current_dir + '/' + job_name + '.txt'
+    copy_file = current_dir + '/' + job_name + '_final.txt'
+
+    if os.path.isfile(output_file):
+        shutil.copy(output_file, copy_file)
+        os.remove(output_file)
+    else:
+        print(f"The original file '{output_file}' does not exist.")
