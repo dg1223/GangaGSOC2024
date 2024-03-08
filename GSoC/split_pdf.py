03631cbfa3e0d1dd382cdc15e61b3c9ef8e08e0b
dg1223 <alavi1223@hotmail.com>
2024-03-07 22:03:00 -0500

    feature: get pdf file name as system argument

--- GSoC/split_pdf.py
+++ GSoC/split_pdf.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
-
 import sys
 import os
 from PyPDF2 import PdfReader, PdfWriter
 
 def get_current_directory():
-    if len(sys.argv) != 2:
-        print(f"You must specify the current directory path as an argument")
+    if len(sys.argv) != 3:
+        print(f"You must specify the current directory path and pdf file name as arguments.")
         sys.exit(1)
 
     current_dir = sys.argv[1]
+    pdf_file = sys.argv[2]
 
-    return current_dir
+    return current_dir, pdf_file
 
 def split_pdf(file):
-    current_dir = get_current_directory()
-    output_folder = current_dir + '/extracted_pages'
+    current_dir, _ = get_current_directory()
+    output_folder = os.path.join(current_dir, 'extracted_pages')
 
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -31,9 +31,10 @@ def split_pdf(file):
             writer.add_page(page)
             writer.write(output_file)
 
-        print(f"Extracted page {page_num+1} from LHC.pdf and saved as {filename}")
+        print(f"Extracted page {page_num+1} from {pdf_file} and saved as {filename}")
 
 
 if __name__ == '__main__':
-    current_dir = get_current_directory()
-    split_pdf(current_dir + '/LHC.pdf')
+    current_dir, pdf_file = get_current_directory()
+    filepath = os.path.join(current_dir, pdf_file)
+    split_pdf(filepath)
