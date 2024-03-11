82004d9780bf85c4c7689598921b41fa631f91ba
dg1223 <alavi1223@hotmail.com>
2024-03-11 10:12:39 -0400

    debug: sleep time with job monitoring update

--- gangagsoc/initial_task.py
+++ gangagsoc/initial_task.py
@@ -13,29 +13,54 @@ split_pdf_script = 'split_pdf.py'
 pdf_file = 'LHC.pdf'
 current_dir = os.getcwd()
 
+def set_current_dir(cur_dir):
+    root_dir = os.path.dirname(cur_dir)
+    parent_dir = 'gangagsoc'
+
+    # for local runs
+    leaf_dir = os.path.basename(cur_dir)
+    if leaf_dir == 'test':
+        cur_dir = os.path.join(root_dir, parent_dir)
+
+    return cur_dir
+
+    # # for CI runs
+    # if not (os.path.exists(wrapper_path) and os.path.exists(main_path)):            
+    #     wrapper_path = os.path.join(current_dir, parent_dir, wrapper_script)
+    #     main_path = os.path.join(current_dir, parent_dir, main_script)
 
 def create_call_script(script=None):
-    if not script and len(sys.argv) != 2:
+    if not script and len(sys.argv) < 2:
         print(f"You must specify the Python file as an argument")
+        print(f"If making a complete system call, \
+            pass the run flag too.")
         sys.exit(1)
 
-    python_script = script if script else sys.argv[1]    
+    python_script = script if script else sys.argv[1]
+    python_script = os.path.basename(python_script)
+
+    cur_dir = set_current_dir(current_dir)
+
+    print()
+    print(f"{python_script = }")
+    print(f"{cur_dir = }")
+    print(f"root dir = {os.path.dirname(cur_dir)}")
 
     # Create a bash script that will run the specified Python script
     with open(call_script, 'w') as script:
         if python_script == word_counting_script:
             script.write(f"""#!/bin/bash
-            python3 "{current_dir}/{python_script}" "$1" "$2" "$3" {pdf_file}
+            python3 "{cur_dir}/{python_script}" "$1" "$2" "$3" {pdf_file}
             """)
         else:
             script.write(f"""#!/bin/bash
-            python3 "{current_dir}/{python_script}" "{current_dir}" {pdf_file}
+            python3 "{cur_dir}/{python_script}" "{cur_dir}" {pdf_file}
             """)
 
     # make it executable
     os.system(f"chmod +x {call_script}")
 
-    return python_script
+    return python_script, cur_dir
 
 def check_file_existence(filepath):    
     if not os.path.exists(filepath):
@@ -43,7 +68,7 @@ def check_file_existence(filepath):
         print("Please store the file in the current directory and rerun the job.\n")
         sys.exit(1)
 
-def submit_ganga_job(python_script):
+def submit_ganga_job(python_script, cur_dir):
     job_name = os.path.splitext(python_script)[0]
     j = Job(name=job_name, backend=Local())
     j.application = Executable()
@@ -51,14 +76,14 @@ def submit_ganga_job(python_script):
 
     # Create splitter for the word count job
     if python_script == word_counting_script:
-        input_pdf = os.path.join(current_dir, pdf_file)
+        input_pdf = os.path.join(cur_dir, pdf_file)
         check_file_existence(input_pdf)
         reader = PdfReader(input_pdf)
         number_of_pages = len(reader.pages)
         word = 'it'
 
         # split using ArgSplitter
-        splitter_args = [ [current_dir, page_num, word] for page_num in range(number_of_pages)]
+        splitter_args = [ [cur_dir, page_num, word] for page_num in range(number_of_pages)]
         j.splitter = ArgSplitter(args=splitter_args)
 
         # merge using TextMerger
@@ -83,7 +108,7 @@ def count_frequency(output_file):
 
     return word_count
 
-def store_word_count(job, job_name, cur_dir=None):
+def store_word_count(job, job_name, cur_dir, cur_dir_from_outside=None):
     '''
     1. Wait (1 min) until job finishes with 'completed' status.
     2. Extract the word counts from the merged file, calculate
@@ -97,22 +122,47 @@ def store_word_count(job, job_name, cur_dir=None):
     with tqdm(total = timeout, \
         leave=False, \
         bar_format='{elapsed}') as progress_bar:
-        while job.status != 'completed':
+
+        '''
+        Running this script externally does not provide direct access
+        to the jobs registry. So, we need to explicitly fetch it.
+        However running it from ganga doesn't have this issue as we 
+        have direct access to the registry there.
+        '''
+        if os.getenv("TEST_SCRIPT_OVERRIDE") == "true":
+            jobs = getRegistryProxy('jobs')
+            current_status = jobs(job.id).status
+
+            if not monitoring_component.enabled:
+                monitoring_component.enable()
+        else:
+            current_status = None
+
+        while current_status != 'completed' and job.status != 'completed':
+            if os.getenv("TEST_SCRIPT_OVERRIDE") == "true":
+                current_status = jobs(job.id).status
+                print(f" {current_status = }")
+
+            print(f" status = {job.status}")
+
             # update progress bar
             elapsed_time = time.time() - start_time
             progress_bar.update(elapsed_time - progress_bar.n)
-
-            # timeout if job is still running, there's probably an issue
+            
+            # timeout if job is still running, can be a monitoring or job issue
             if elapsed_time > timeout:
-                print("Timeout reached. Cannot print results. Exiting job...")
-                return
+                if job.status == 'completed':
+                    break
+                else:
+                    print("Timeout reached. Job didn't finish. Exiting job...")
+                    return
 
             time.sleep(1)
 
     merged_output = job.outputdir + 'stdout'
     word_count = count_frequency(merged_output)
 
-    current_dir = os.getcwd() if not cur_dir else current_dir
+    current_dir = os.getcwd() if not cur_dir_from_outside else cur_dir
     result_file = current_dir + '/' + job_name + '.txt'
 
     with open(result_file, 'w') as f:
@@ -124,28 +174,39 @@ def store_word_count(job, job_name, cur_dir=None):
     print(f"\nRun this command to check the output from TextMerger: jobs({job.id}).peek('stdout')")
 
 def execute_initial_task():
-    script = create_call_script()
-    job, job_name = submit_ganga_job(script)
+    script, cur_dir = create_call_script()
+    job, job_name = submit_ganga_job(script, cur_dir)
 
     if script == word_counting_script:
-        store_word_count(job, job_name)
+        store_word_count(job, job_name, cur_dir)
     elif script == split_pdf_script:
-        print(f"\nExtracted pages from {pdf_file} have been saved in the folder {current_dir}/extracted_pages")
+        print(f"\nExtracted pages from {pdf_file} have been saved in the folder {cur_dir}/extracted_pages")
         print(f"\nFor a detailed stdout, run the command: jobs({job.id}).peek('stdout')")
     
     try:
         os.remove('run_initial_task.sh')
     except FileNotFoundError:
-        print(f"run_initial_task.sh not found in {current_dir}")
+        print(f"run_initial_task.sh not found in {cur_dir}")
 
 
 # Prevent autorun if script is being imported by test_InitialTask.py
-if os.getenv("FROM_TEST_SCRIPT") == "true" or os.getenv("FROM_INIT") == "true":
+if os.getenv("FROM_TEST_SCRIPT") == "true":
     RUN_INITIAL_TASK = False
 else:
     RUN_INITIAL_TASK = True
-    
+
+# TEST_SCRIPT_OVERRIDE helps mimic an entire system call from test scripts
 if RUN_INITIAL_TASK:
+    execute_initial_task()
+elif os.getenv("TEST_SCRIPT_OVERRIDE") == "true":
+    from ganga.ganga import ganga
+    from ganga import Job, Local, Executable, File, ArgSplitter, TextMerger
+    '''
+    Importing sleep_until_completed() here throws a ConfigError,
+    so I have used some of its logic to implements a sleep timer myself
+    '''
+    from GangaCore.Core.GangaRepository import getRegistryProxy
+    from GangaCore.Core import monitoring_component
     execute_initial_task()
 else:
     from ganga.ganga import ganga
