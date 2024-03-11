69477022d4821e24dd8d454d42cd51f49649b7bd
dg1223 <alavi1223@hotmail.com>
2024-03-11 06:52:30 -0400

    test: added tests for count_it.py

---
+++ test/test_CountIt.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import unittest
+from fpdf import FPDF
+from ganga.GangaTest.Framework.utils import sleep_until_completed
+
+os.environ["FROM_TEST_SCRIPT"] = "true"
+
+wrapper_script = 'run_initial_task.sh'
+word_counting_script = 'count_it.py'
+
+class TestCountIt(unittest.TestCase):
+    def testGetArguments(self):
+        from gangagsoc.count_it import get_arguments
+
+        sys.argv = ["count_it.py", "current_dir", "page_num", "word", "pdf_file"]
+        self.assertEqual(get_arguments(), ("current_dir", "page_num", "word", "pdf_file"))
+
+    def testPreprocessText(self):
+        from gangagsoc.count_it import preprocess_text
+
+        text = "The Large Hadron Collider! [citation] {CERN} more TEXT."
+        self.assertEqual(preprocess_text(text), "the large hadron collider  citation  cern more text")
+
+    def testCountWord(self):
+        from gangagsoc.count_it import count_word
+
+        pdf_file = 'test_pdf.pdf'
+        pdf_writer = FPDF()
+        pdf_writer.add_page()
+        pdf_writer.set_xy(0, 0)
+        pdf_writer.set_font('Times')
+        text = "The Large Hadron Collider at CERN aka LHC aka large-hadron-collider."
+        pdf_writer.cell(ln=0, align='L', w=0, txt=text, border=0)        
+        pdf_writer.output(pdf_file, 'F')
+
+        self.assertEqual(count_word(pdf_file, "0", "hadron"), 1)
+        os.remove(pdf_file)
