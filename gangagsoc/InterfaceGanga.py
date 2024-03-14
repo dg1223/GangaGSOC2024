a051aa11939bf0614397434992cc26779003bac8
dg1223 <alavi1223@hotmail.com>
2024-03-14 11:53:48 -0400

    feature: complete code parser from LLM output

--- gangagsoc/InterfaceGanga.py
+++ gangagsoc/InterfaceGanga.py
@@ -1,6 +1,8 @@
 import re
+import sys
 import time
 import torch
+import numpy as np
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 class InterfaceGanga:
@@ -22,11 +24,11 @@ class InterfaceGanga:
         # Run on GPU if available
         if torch.cuda.is_available():
             print("\nFound CUDA compatible GPU. Utilizing GPU...\n\
-                Esimated runtime: < 1 minute\n")
+Esimated runtime: < 1 minute\n")
             model = model.cuda()
         else:
             print("\nNo CUDA compatible GPU found. Running on CPU only...\n\
-                Esimated runtime: < 13-25 minutes\n")
+Esimated runtime: < 13-25 minutes\n")
 
         if not tokenizer.pad_token:
             tokenizer.pad_token = tokenizer.eos_token
@@ -46,6 +48,8 @@ class InterfaceGanga:
 
         return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
+    ### THIS FUNCTION BLOCK IS AUXILLARY TO THE PROBLEM STATEMENT ###
+
     def store_llm_output(self, output):
         output_file = 'llm_output.txt'
         with open(output_file, 'w') as f:
@@ -62,21 +66,98 @@ class InterfaceGanga:
     def print_llm_output(self, llm_output):
         print(llm_output)
 
-    def parse_llm_output(self, llm_output):
-        python_code_matches = re.findall(r'```python\s+(.*?)\s+```', llm_output, re.DOTALL)
-        bash_code_matches = re.findall(r'```bash\s+(.*?)\s+```', llm_output, re.DOTALL)
+    ### THIS FUNCTION BLOCK IS AUXILLARY TO THE PROBLEM STATEMENT ###
 
-        if len(python_code_matches) > 0:
-            for idx, python_code in enumerate(python_code_matches):
-                print(f"python code snippet #{idx+1}:\n{python_code}\n\n")
-        else:
-            print("\nNo Python code snippet could be parsed from LLM's output.\n")
 
-        if len(bash_code_matches) > 0:
-            for idx, bash_code in enumerate(bash_code_matches):
-                print(f"bash code snippet:\n{bash_code}\n\n")
+    def extract_code_snippet(self, pattern_type, pattern_1, pattern_2, llm_output):
+        total_tries_left = 2
+        while total_tries_left > 0:
+            if total_tries_left == 2:
+                snippets = re.findall(pattern_1, llm_output, re.DOTALL)
+            else:
+                snippets = re.findall(pattern_2, llm_output, re.DOTALL)
+                    
+            snippet = self.helper_extract_code_snippet(snippets)
+
+            if not snippet:
+                total_tries_left -= 1
+                if total_tries_left == 1:
+                    print(f"\nWARNING: No code snippet for '{pattern_type}' was parsed \
+using coding marker #1.\nLLM probably generated a different pattern of text.")
+                    print("Trying a different coding marker to match pattern...")            
+                    continue
+                # No point running a Ganga job when there's no code to run
+                elif pattern_type == 'Pi approximation' or pattern_type == 'Ganga job':
+                    print("\nERROR: Unable to parse code from LLM's output. Exiting...\n")
+                    sys.exit(1)
+                else:
+                    print("LLM generated incomplete code. Continuing...")
+            else:
+                print(f"\n'SUCCESS!! {pattern_type}' code found in LLM's output.")
+
+            return snippet
+
+    def helper_extract_code_snippet(self, snippets):
+        snippet_size = np.shape(snippets)
+        dimensions = len(snippet_size)
+        # no dimension
+        if not snippet_size or snippet_size[0] == 0:
+            return None
+        # one-dimensional
+        elif dimensions == 1 and snippet_size[0] > 0:
+            snippet = snippets[0]
+        # two-dimensional
         else:
-            print("\nNo Bash code snippet could be parsed from LLM's output.\n")
+            snippet = snippets[0][0]
+
+        return snippet
+
+    def write_code_snippet_to_file(self, llm_output):
+        '''
+        These hardcoded patterns were developer after doing multiple simulation runs
+        with the same prompt on the deepseeker model. However, the LLM's consistency
+        cannot be guaranteed.
+        '''
+        pi_pattern_1 = r'python code snippet #1:(.*?)((?=\npython code snippet #2)|$)'
+        ganga_pattern_1 = r'python code snippet #2:(.*?)((?=\nbash code snippet)|$)'
+        bash_pattern_1 = r'bash code snippet:(.*?)((?=\npython code snippet)|$)'
+        pi_pattern_2 = r"```python(.*?)```"
+        ganga_pattern_2 = r"```python(\n[^`]*?from.*?)```"
+        bash_pattern_2 = r"```bash(.*?)```"
+
+        python_snippet = self.extract_code_snippet(\
+            'Pi approximation', pi_pattern_1, pi_pattern_2, llm_output)
+        print(python_snippet)
+        ganga_snippet = self.extract_code_snippet(\
+            'Ganga job', ganga_pattern_1, ganga_pattern_2, llm_output)
+        print(ganga_snippet)
+        bash_snippet = self.extract_code_snippet(\
+            'Bash', bash_pattern_1, bash_pattern_2, llm_output)
+        print(bash_snippet)
+
+        # all code snippets should be ready by this point. #
+        
+        # Generate file names
+        pi_function_name = re.findall(r'def\s+(\w+)\(', python_snippet, re.DOTALL)
+        if pi_function_name:
+            if len(pi_function_name) > 1:
+                pi_filename = f"{pi_function_name[0]}.py"        
+            else:
+                pi_filename = f"{pi_function_name}.py"
+        else:
+            pi_filename = "pi_estimation.py"
+
+        ganga_filename = "run_ganga_job.py"
+        bash_filename = "run_ganga.sh"
+
+        # Write code snippets to respective files
+        self.helper_write_to_file(pi_filename, python_snippet)
+        self.helper_write_to_file(ganga_filename, ganga_snippet)
+        self.helper_write_to_file(bash_filename, bash_snippet)
+
+    def helper_write_to_file(self, filename, snippet):
+        with open(filename, 'w') as file:
+            file.write(snippet.lstrip())
 
 
 if __name__ == '__main__':
@@ -102,8 +183,4 @@ if __name__ == '__main__':
 
     llm = InterfaceGanga(llm_input=prompt)
     output = llm.run_llm()
-    output_file = llm.store_llm_output(output)
-    content = llm.read_llm_output(output_file)
-    llm.print_llm_output(content)
-    print("\n\n")
-    llm.parse_llm_output(content)
+    llm.write_code_snippet_to_file(output)
