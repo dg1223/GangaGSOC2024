788b97d67284c71fb5cc1ae8b70a0383420f9caa
dg1223 <alavi1223@hotmail.com>
2024-03-14 17:30:13 -0400

    test: added message on assertion failure

--- test/test_CountIt.py
+++ test/test_CountIt.py
@@ -14,13 +14,16 @@ class TestCountIt(unittest.TestCase):
         from gangagsoc.count_it import get_arguments
 
         sys.argv = ["count_it.py", "current_dir", "page_num", "word", "pdf_file"]
-        self.assertEqual(get_arguments(), ("current_dir", "page_num", "word", "pdf_file"))
+        self.assertEqual(get_arguments(),\
+            ("current_dir", "page_num", "word", "pdf_file"),\
+                "Arguments retrieved incorrectly.")
 
     def testPreprocessText(self):
         from gangagsoc.count_it import preprocess_text
 
         text = "The Large Hadron Collider! [citation] {CERN} more TEXT."
-        self.assertEqual(preprocess_text(text), "the large hadron collider  citation  cern more text")
+        self.assertEqual(preprocess_text(text),
+        \"the large hadron collider  citation  cern more text", "Text preprocessing incorrect.")
 
     def testCountWord(self):
         from gangagsoc.count_it import count_word
@@ -34,5 +37,5 @@ class TestCountIt(unittest.TestCase):
         pdf_writer.cell(ln=0, align='L', w=0, txt=text, border=0)        
         pdf_writer.output(pdf_file, 'F')
 
-        self.assertEqual(count_word(pdf_file, "0", "hadron"), 1)
+        self.assertEqual(count_word(pdf_file, "0", "hadron"), 1, "Word count incorrect.")
         os.remove(pdf_file)
